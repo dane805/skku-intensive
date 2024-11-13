59047b97ff2ef303dcf0b093108ad91bcc53b98b
changbae.ahn <changbae.ahn@ilevit.com>
2024-11-14 02:50:00 +0900

    데이터 추가 및 팀별 기초 템플릿

---
+++ team1/app.py
@@ -0,0 +1,24 @@
+from glob import glob
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import streamlit as st
+
+df = pd.read_parquet("monthly_review_count.parquet")
+df.review_month = df.review_month.dt.to_timestamp()
+
+target_stores = df.store_name.unique()
+
+
+store_name = st.selectbox(
+    '리뷰 내용이 궁금한 레스토랑을 선택해주세요',
+    target_stores)
+
+fig = plt.figure()
+plt.scatter(data=df[df.store_name == store_name], x='review_month', y='size')
+plt.xlabel('Month')
+plt.ylabel('Review count')
+plt.xticks(rotation=20)
+st.pyplot(fig)
+
+# streamlit run app.py --server.port 80
---
+++ team3/app.py
@@ -0,0 +1,38 @@
+from glob import glob
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import streamlit as st
+
+df = pd.read_parquet("monthly_review_count.parquet")
+df_meta = pd.read_parquet("df_meta.parquet")
+
+df.review_month = df.review_month.dt.to_timestamp()
+
+target_stores = df.store_name.unique()
+
+
+store_name = st.selectbox(
+    '리뷰 내용이 궁금한 레스토랑을 선택해주세요',
+    target_stores)
+
+
+store_cate = df_meta.loc[df_meta.store_name == store_name, "store_cate"].values[0]
+store_location = df_meta.loc[df_meta.store_name == store_name, "store_location"].values[0]
+
+con1, con2 = st.columns([0.5,0.5])
+
+with con1:
+    map_data = pd.DataFrame(
+        [[37.4900861966504, 127.01953478052]],
+        columns=['lat', 'lon'])
+    st.map(map_data)
+
+with con2:
+    st.subheader(f"식당 이름: {store_name}")
+    st.text(f"식당 종류: {store_cate}")
+    st.text(f"식당 위치: {store_location}")
+
+# streamlit run app.py --server.port 80
+# 
